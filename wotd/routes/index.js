3ae6504b64f80a12e4ae3aa2ed5a073f9422dbbb
Kyle Rush <krush@sonobello.com>
2018-06-04 20:35:11 -0700

    nodejs working, processing rss feed, working on layout

--- wotd/routes/index.js
+++ wotd/routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
+var request = require('request');
+var parser = require('rss-to-json');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	var url = 'https://wordsmith.org/awad/rss1.xml';
+	parser.load(url, function(error, rss) {
+		console.log("RSS: " + JSON.stringify(rss));
+		//parsedRes = parser.parseString(body);
+		//console.log(parsedRes);
+		var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+		var today  = new Date();
+
+		todayFormatted = today.toLocaleDateString("en-US", options); // Saturday, September 17, 2016
+
+		res.render('index', { title: todayFormatted, word: rss['items'][0]['title'], definition: rss['items'][0]['description']});
+	});
+  
 });
 
 module.exports = router;
