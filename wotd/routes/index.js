362fc0ca2585b30cb8c75caad018d90266e381e8
Kyle Rush <krush@sonobello.com>
2018-09-12 19:36:50 -0700

    Ticket rate

--- wotd/routes/index.js
+++ wotd/routes/index.js
@@ -5,6 +5,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+	var ticketRateUrl = 'https://2g70tu7zpb.execute-api.us-west-2.amazonaws.com/Prod';
+
 	var url = 'https://wordsmith.org/awad/rss1.xml';
 	parser.load(url, function(error, rss) {
 		console.log("RSS: " + JSON.stringify(rss));
@@ -12,10 +14,13 @@ router.get('/', function(req, res, next) {
 		//console.log(parsedRes);
 		var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
 		var today  = new Date();
-
+		var ticketRateTitle = "Tickets posted in the past hour:";
 		todayFormatted = today.toLocaleDateString("en-US", options); // Saturday, September 17, 2016
+		request(ticketRateUrl, function(error, data) {
+			res.render('index', { title: todayFormatted, word: rss['items'][0]['title'], definition: rss['items'][0]['description'], ticketTitle: ticketRateTitle, ticketRate: data.body});
+		});
 
-		res.render('index', { title: todayFormatted, word: rss['items'][0]['title'], definition: rss['items'][0]['description']});
+		
 	});
   
 });
